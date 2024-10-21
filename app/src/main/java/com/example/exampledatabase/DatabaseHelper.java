4f58f662f980937cdff8bad15a171bffd4fc2f31
31032019 <jpcostac17@gmail.com>
2024-10-21 16:25:14 -0300

    Create methods to insert profiles, list profiles, and list contacts with profiles.

--- app/src/main/java/com/example/exampledatabase/DatabaseHelper.java
+++ app/src/main/java/com/example/exampledatabase/DatabaseHelper.java
@@ -3,8 +3,10 @@ package com.example.exampledatabase;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
@@ -18,26 +20,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable = "CREATE TABLE contatos(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, telefone TEXT, email TEXT)";
+
+        String createTablePerfil ="CREATE TABLE perfil(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, url TEXT, fkContato INTEGER, FOREIGN KEY (fkContato) REFERENCES contato(id))";
+
         db.execSQL(createTable);
+        db.execSQL(createTablePerfil);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS contatos");
+        db.execSQL("DROP TABLE IF EXISTS perfil");
         onCreate(db);
     }
 
     //Método para inserir um contato
-    public boolean inserirContato(String nome, String telefone, String email){
+    public boolean inserirContato(String nomeContato, String telefone, String email, String nomePerfil, String url){
         SQLiteDatabase db= this.getWritableDatabase();
         ContentValues values = new ContentValues();
+        long resultado = 0;
 
-        values.put("nome", nome);
+        values.put("nome", nomeContato);
         values.put("telefone", telefone);
         values.put("email", email);
 
-        long resultado = db.insert("contatos", null, values);
-        db.close();
+        try{
+            db.insert("contatos", null, values);
+            this.inserirPerfil(nomePerfil, url, id);
+        }catch (SQLException e){
+            Log.e("Erro",e.getMessage());
+            resultado = -1;
+        }finally {
+            db.close();
+        }
 
         return resultado != -1;
     }
@@ -63,11 +78,41 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return resultado > 0;
     }
 
+    //Método para excluir um contato
     public boolean excluirContato(int id){
         SQLiteDatabase db = this.getWritableDatabase();
         long resultado = db.delete("contatos", "id = ?", new String[]{String.valueOf(id)});
 
         db.close();
         return  resultado > 0;
+    }
+
+    //Método para listar os perfis de um contato
+    public Cursor listarPerfis(int idContato){
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        return db.rawQuery("SELECT nome, url FROM perfil WHERE fkContato =?", new String[]{String.valueOf(idContato)});
+    }
+
+    //Método para listar os contatos com seus respectivos perfis
+    public Cursor listarContatosComPerfil(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("SELECT c.id, c.nome, c.telefone, c.email, p.nome, p.url FROM contatos c" +
+                " JOIN perfil p ON c.id = p.fkContato ORDER BY c.nome", null);
+    }
+
+    //Método para inserir um perfil
+    public boolean inserirPerfil(String nome, String url, int idContato){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("nome", nome);
+        values.put("url", url);
+        values.put("fkContato", idContato);
+
+        long resultado = db.insert("perfil", null, values);
+        db.close();
+
+        return resultado != -1;
     }
 }
